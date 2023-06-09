7904b8cc1fc28f6842ceb7310515269380bbcff3
huanghaian <huanghaian@sensetime.com>
2023-06-09 20:41:41 +0800

    update3

---
+++ dist_eval.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+CFG=$1
+W=$2
+GPUS=$3
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch \
+    --nnodes=$NNODES \
+    --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR \
+    --nproc_per_node=$GPUS \
+    --master_port=$PORT \
+    $(dirname "$0")/eval.py \
+    evaluate --conf_files $CFG \
+    --overrides WEIGHT $W
+    --launcher pytorch \
